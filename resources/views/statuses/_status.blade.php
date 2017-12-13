cd6a7d418378af9dd86cb49c73742212748548b1
iej <iej@123.com>
2017-12-13 10:22:29 +0800

    test

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,7 +1,6 @@
 <li id="status-{{ $status->id }}">
     <a href="{{ route('users.show', $user->id )}}">
-        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}"
-             class="gravatar"/>
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
     </a>
     <span class="user">
     <a href="{{ route('users.show', $user->id )}}">{{ $user->name }}</a>
@@ -10,4 +9,11 @@
     {{ $status->created_at->diffForHumans() }}
   </span>
     <span class="content">{{ $status->content }}</span>
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger status-delete-btn">删除</button>
+        </form>
+    @endcan
 </li>
